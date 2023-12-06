7b023146f6c7472a8c8858e5ae975df936dc8f88
Jeric Wandasan <jtwandasan@gmail.com>
2023-12-06 15:17:47 -0700

    still separating data

--- housing_model.py
+++ housing_model.py
@@ -2,11 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
-# Reading a csv file into workable format
+test_df = pd.read_csv("test.csv")
+train_df = pd.read_csv("train.csv")
+
+# Reads a column
+column_with_data = {}
+
+train_columns = train_df.columns
+for i in train_columns:
+    column_with_data[i] = train_df[i]
+
+print(column_with_data)
+
+# Reads each row with all of its data
 all_rows = []
 my_dict = {}
 id_with_row = {}
-column_with_row = {}
 
 with open('test.csv', newline='\n') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ')
@@ -15,14 +26,15 @@ with open('test.csv', newline='\n') as csvfile:
             row_split = i.split(",")
             all_rows.append(row_split)
 
-# for i in all_rows[1:]:
-#     for j in i[1:]:
-#         id_with_row{f"{}"}
+for i in all_rows[1:]:
+    id_with_row[i[0]] = i[1:]
 
 
 
-# test_df = pd.read_csv("test.csv")
-# train_df = pd.read_csv("train.csv")
+
+
+
+
 
 # print(test_df.head())
 # print(train_df.head())
