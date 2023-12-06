c0e56f07ef608669c5a27e04f447758f42437160
Jeric Wandasan <jtwandasan@gmail.com>
2023-12-06 16:41:59 -0700

    Updated cleaning

--- housing_model.py
+++ housing_model.py
@@ -2,41 +2,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
+def clean_conditionals(df):
+    sep_cols = ["MSZoning","LotShape","LandContour","Utilities","LotConfig","Neighborhood","Condition1","Condition2","BldgType","HouseStyle","RoofStyle","RoofMatl","Exterior1st","Exterior2nd","MasVnrType","Foundation","Heating","Electrical","GarageType","PavedDrive","Fence","MiscFeature","SaleType","SaleCondition"]
+
+    df_encoded = pd.DataFrame()
+
+    # Give conditionals their own column of True or False
+    for i in sep_cols:
+        encoded = pd.get_dummies(df, columns=[i], prefix = i)
+        df_encoded = pd.concat([df_encoded, encoded], axis=1)
+        
+    # Remove duplicates
+    df_encoded = df_encoded.loc[:, ~df_encoded.columns.duplicated()]
+    # Change all Trues and Falses to 1 and 0 respectively
+    df_encoded = df_encoded.map(lambda x: 1 if x == True else (0 if x == False else x))
+    # Output to CSV file to check
+    df_encoded.drop(sep_cols, axis = 1, inplace=True)
+    df_encoded.to_csv('encoded_df.csv', index=False)
+
+    return df_encoded
+
+# Read in data initially
 test_df = pd.read_csv("test.csv")
 train_df = pd.read_csv("train.csv")
 
-# Reads a column
-column_with_data = {}
+train_df_encoded = clean_conditionals(train_df)
+test_df_encoded = clean_conditionals(test_df)
 
-train_columns = train_df.columns
-for i in train_columns:
-    column_with_data[i] = train_df[i]
+# The following will clean the rest of the data
 
-print(column_with_data)
-
-# Reads each row with all of its data
-all_rows = []
-my_dict = {}
-id_with_row = {}
-
-with open('test.csv', newline='\n') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ')
-    for row in reader:
-        for i in row:
-            row_split = i.split(",")
-            all_rows.append(row_split)
-
-for i in all_rows[1:]:
-    id_with_row[i[0]] = i[1:]
-
-
-
-
-
-
-
-
-# print(test_df.head())
-# print(train_df.head())
-df_encoded = pd.get_dummies(train_df, columns=['HouseStyle'], prefix='HouseStyle', drop_first=True)
 
