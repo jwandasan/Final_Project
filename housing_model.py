ecb6f2771244103d7e6a32d1dcda5d0bf4b67805
Jeric Wandasan <jtwandasan@gmail.com>
2023-12-06 14:53:57 -0700

    started reading csv file into workable format

--- housing_model.py
+++ housing_model.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import csv
 
-test_df = pd.read_csv("test.csv")
-train_df = pd.read_csv("train.csv")
+# Reading a csv file into workable format
+all_rows = []
+my_dict = {}
+id_with_row = {}
+column_with_row = {}
 
-print(test_df.head())
-print(train_df.head())
+with open('test.csv', newline='\n') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    for row in reader:
+        for i in row:
+            row_split = i.split(",")
+            all_rows.append(row_split)
+
+# for i in all_rows[1:]:
+#     for j in i[1:]:
+#         id_with_row{f"{}"}
+
+
+
+# test_df = pd.read_csv("test.csv")
+# train_df = pd.read_csv("train.csv")
+
+# print(test_df.head())
+# print(train_df.head())
 
