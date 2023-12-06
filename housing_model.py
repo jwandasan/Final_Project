a82de0b4bfd38fd308f56ce56c7b1b358db3481f
jachristensen2020 <jachristensen2020@gmail.com>
2023-12-06 16:47:53 -0700

    Rest of the cleaning code

--- housing_model.py
+++ housing_model.py
@@ -30,5 +30,67 @@ train_df_encoded = clean_conditionals(train_df)
 test_df_encoded = clean_conditionals(test_df)
 
 # The following will clean the rest of the data
+# Street Column
+d_street = {'Grvl': 1, 'Pave':2}
+X_train['Street'] = X_train['Street'].apply(d_street.get)
 
+# Alley Column
+d_alley = {np.nan: 0, 'Grvl': 1, 'Pave': 2}
+X_train['Alley'] = X_train['Alley'].apply(d_alley.get)
+
+# LandSlope Column
+d_lslope = {'Gtl': 1, 'Mod': 2, 'Sev': 3}
+X_train['LandSlope'] = X_train['LandSlope'].apply(d_lslope.get)
+
+# ExterQual Column
+d_exter = {'Po': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5}
+X_train['ExterQual'] = X_train['ExterQual'].apply(d_exter.get)
+
+# ExterCond Column
+X_train['ExterCond'] = X_train['ExterCond'].apply(d_exter.get)
+
+# BsmtQual Column
+d_bsmt = {np.nan: 0, 'Po': 1, 'Fa': 2, 'TA': 3, 'Gd': 4, 'Ex': 5}
+X_train['BsmtQual'] = X_train['BsmtQual'].apply(d_bsmt.get)
+
+# BsmtCond Column
+X_train['BsmtCond'] = X_train['BsmtCond'].apply(d_bsmt.get)
+
+# BsmtExposure
+d_bsmtexp = {np.nan: 0, 'No': 1, 'Mn': 2, 'Av': 3, 'Gd': 4}
+X_train['BsmtExposure'] = X_train['BsmtExposure'].apply(d_bsmtexp.get)
+
+#BsmtFinType1 Column
+d_bsmtfin1 = {np.nan: 0, 'Unf': 1, 'LwQ': 2, 'Rec': 3, 'BLQ': 4, 'ALQ': 5, 'GLQ': 6}
+X_train['BsmtFinType1'] = X_train['BsmtFinType1'].apply(d_bsmtfin1.get)
+
+# BsmtFinType2 Column
+X_train['BsmtFinType2'] = X_train['BsmtFinType2'].apply(d_bsmtfin1.get)
+
+# HeatingQC Column
+d_heat = d_exter
+X_train['HeatingQC'] = X_train['HeatingQC'].apply(d_heat.get)
+
+#CentralAir Column
+d_air = {'Y': 1, 'N': 0}
+X_train['CentralAir'] = X_train['CentralAir'].apply(d_air.get)
+
+#KitchenQual Column
+d_kitchqual = d_heat
+X_train['KitchenQual'] = X_train['KitchenQual'].apply(d_kitchqual.get)
+
+# FireplaceQu Column
+d_fire = d_bsmt
+X_train['FireplaceQu'] = X_train['FireplaceQu'].apply(d_fire.get)
+
+# GarageQual Column
+d_garage = d_fire
+X_train['GarageQual'] = X_train['GarageQual'].apply(d_garage.get)
+
+# GarageCond Column
+X_train['GarageCond'] = X_train['GarageCond'].apply(d_garage.get)
+
+# PoolQC Column
+d_pool = {np.nan: 0, 'Fa': 1, 'TA': 2, 'Gd': 3, 'Ex': 4}
+X_train['PoolQC'] = X_train['PoolQC'].apply(d_pool.get)
 
